090c4d6519d3f85a0a2d6cf1e3438b9ce41512fa
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-07-31 23:10:27 -0500

    Reveal Implimented

---
+++ Smack/ChatVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Chris McDonald on 7/31/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    @IBOutlet weak var menuButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)) , for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+}
