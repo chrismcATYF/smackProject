c5f9a668103d07d6481432fcd6173b7496c0a8c2
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-01 21:32:52 -0500

    added login

---
+++ Smack/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Chris McDonald on 8/1/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
---
+++ Smack/loginVC.swift
@@ -0,0 +1,22 @@
+//
+//  loginVC.swift
+//  Smack
+//
+//  Created by Chris McDonald on 8/1/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
