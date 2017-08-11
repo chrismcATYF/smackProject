4b881522a9f18b2efd461bd58295c81c7c7b2783
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-10 21:15:19 -0500

    channel backbones created

---
+++ Smack/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Smack
+//
+//  Created by Chris McDonald on 8/10/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    private(set) public var channelTitle: String!
+    private(set) public var channelDescription: String!
+    private(set) public var id: String!
+}
