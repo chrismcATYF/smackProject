d06aefaf62b7a1f69f308a5e08506eacee3ed836
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-10 22:50:24 -0500

    More functionality to channel

---
+++ Smack/ChannelCell.swift
@@ -0,0 +1,34 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Chris McDonald on 8/10/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func configureCell(channel: Channel) {
+        let title = channel.channelTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+    
+}
