55d4b023d924188bf0dcaac83c1f5a198495fbcf
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-13 20:46:56 -0500

    channel titles work

--- Smack/ChatVC.swift
+++ Smack/ChatVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChatVC: UIViewController {
     
     @IBOutlet weak var menuButton: UIButton!
+    @IBOutlet weak var channelNameLbl: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,13 +19,38 @@ class ChatVC: UIViewController {
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
         
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNEL_SELECTED, object: nil)
+        
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail(completion: { (success) in
                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
             })
         }
+    }
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            onLoginGetMessages()
+        } else {
+            channelNameLbl.text = "Please Login"
+        }
+    }
+    
+    @objc func channelSelected(_ notif: Notification) {
+        updateWithChannel()
+    }
+    
+    func updateWithChannel() {
+        let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
+        channelNameLbl.text = "#\(channelName)"
+    }
+    
+    func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in
-            
+            if success {
+                
+            }
         }
     }
 }
