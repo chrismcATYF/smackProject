2a8a5f912260a8bce160b011918e805b1f1a302d
Chris McDonald <chrismcdonald@Chriss-MacBook-Air.local>
2017-08-14 20:40:48 -0500

    chat display added

---
+++ Smack/MessageCell.swift
@@ -0,0 +1,30 @@
+//
+//  MessageCell.swift
+//  Smack
+//
+//  Created by Chris McDonald on 8/14/17.
+//  Copyright © 2017 Chris McDonald. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var userImage: CirlceImage!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var timeStampLbl: UILabel!
+    @IBOutlet weak var messageBodyLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func configureCell(message: Message) {
+        messageBodyLbl.text = message.message
+        userNameLbl.text = message.username
+        userImage.image = UIImage(named: message.userAvatar)
+        userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+    }
+    
+}
